24a365fb8c1f693c36f64faaee110cfd02e4df34
Max Patiiuk <max@patii.uk>
2021-01-30 23:31:26 +0200

    Initial commit
    World, be prepared to be surprised

---
+++ components/InteractivePrivitives.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+const baseButtonStyle = 'inline-flex px-4 py-2 rounded-md sm:text-sm sm:w-auto text-gray-700';
+
+export const ButtonSecondary = ({
+	children,
+	props={},
+	extraStyles=''
+}:{
+	children: React.ReactNode,
+	props?: Record<string,unknown>
+	extraStyles?: string,
+}) =>
+	<button
+		type="button"
+		className={`bg-white hover:bg-grey-700 border ${baseButtonStyle} ${extraStyles}`}
+		{...props}
+	>
+		{children}
+	</button>;
+
+export const ButtonDanger = ({
+	children,
+	props={},
+	extraStyles=''
+}:{
+	children: React.ReactNode,
+	props?: Record<string,unknown>
+	extraStyles?: string,
+}) =>
+	<button
+		type="button"
+		className={`bg-red-600 hover:bg-red-700 ${extraStyles}`}
+		{...props}
+	>
+		{children}
+	</button>;
