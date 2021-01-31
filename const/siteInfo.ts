4109c20dd1c0ff9f7828e8374a9712396119ab6f
Max Patiiuk <max@patii.uk>
2021-01-31 02:24:52 +0200

    Add favicons of varions sizes and `site.manifest`

--- const/siteInfo.ts
+++ const/siteInfo.ts
@@ -1,4 +1,5 @@
 import { LanguageStringsStructure } from '../lib/languages';
+import { strip } from '../lib/localizationHelper';
 
 interface ErrorPageLocalization extends LanguageStringsStructure {
 	'en-US': {
@@ -12,12 +13,12 @@ interface ErrorPageLocalization extends LanguageStringsStructure {
 const SiteInfo: ErrorPageLocalization = {
 	'en-US': {
 		title: 'TTS King',
-		description: `Convert your daily news digests into a simple podcast you can listen to
+		description: strip(`Convert your daily news digests into a simple podcast you can listen to
 			while in transit, walking or even exercising. TTS King helps you stay productive
-			no matter where you are!`,
-		keywords: `TTS King, tts, text-to-speech, text to speech, convert websites to audio,
+			no matter where you are!`),
+		keywords: strip(`TTS King, tts, text-to-speech, text to speech, convert websites to audio,
 			website to audio, website to audiobook, reader mode, website reader mode, distraction
-			free browser`,
+			free browser`),
 		author: 'Maksym Patiiuk',
 	},
 };
