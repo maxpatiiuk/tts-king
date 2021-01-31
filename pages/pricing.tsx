3686afcc94b18d6a42472d3c86681214792c4065
Max Patiiuk <max@patii.uk>
2021-01-31 02:51:43 +0200

    Finish menu and create boilerplate pages

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,12 +1,14 @@
 import Layout         from '../components/Layout';
 import { PublicMenu } from '../components/PublicMenu';
-import Link           from 'next/Link';
 import { Centered }   from '../components/UI';
 
 const Home = () =>
 	<Layout>
 		<PublicMenu />
-		<Centered>1</Centered>
+		<Centered>
+			<h1 className='text-9xl font-bold'>It's free! For now...</h1>
+			<p className='text-xl'>Enjoy it while you can</p>
+		</Centered>
 	</Layout>;
 
 export default Home;
