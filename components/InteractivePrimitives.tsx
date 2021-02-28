71a56db90720a988dcc5d24c5dc87f735471b77b
Max Patiiuk <max@patii.uk>
2021-02-28 04:15:26 +0200

    Replace interactive primitive components with classes

--- components/InteractivePrimitives.tsx
+++ components/InteractivePrimitives.tsx
@@ -1,62 +1,19 @@
-import React from 'react';
+import React     from 'react';
 
 const baseButtonStyle = 'inline-flex px-4 py-2 rounded-md sm:text-sm' +
 	' sm:w-auto text-gray-700';
+export const primaryButtonClassName = `${baseButtonStyle} bg-white
+	hover:bg-grey-500 border`;
+export const secondaryButtonClassName = `${baseButtonStyle} bg-grey-200
+	hover:bg-grey-700 border`;
+export const dangerButtonClassName = `${baseButtonStyle} bg-red-600
+	hover:bg-red-700`;
+export const successButtonClassName = `${baseButtonStyle} bg-green-600
+	hover:bg-green-700`;
+export const dangerLinkClassName = `border-none p-none m-none text-red-500
+	hover:underline text-left`;
 
-interface ButtonProps {
-	children: React.ReactNode,
-	props?: React.ButtonHTMLAttributes<HTMLButtonElement>
-	extraStyles?: string,
-	baseStyles?: string,
-}
-
-function Button({
-	children,
-	props = {},
-	baseStyles = '',
-	extraStyles = '',
-}:ButtonProps ){
-	return <button
-		type="button"
-		className={`${baseStyles} ${baseButtonStyle} ${
-			extraStyles}`}
-		{...props}
-	>
-		{children}
-	</button>;
-}
-
-export function ButtonPrimary({children, ...props}:ButtonProps) {
-	return <Button
-		baseStyles={'bg-white hover:bg-grey-500 border'}
-		{...props}>
-		{children}
-	</Button>
-}
-
-export function ButtonSecondary({children, ...props}:ButtonProps) {
-	return <Button
-		baseStyles={'bg-grey-200 hover:bg-grey-700 border'}
-		{...props}>
-		{children}
-	</Button>
-}
-
-export function ButtonDanger({children, ...props}:ButtonProps) {
-	return <Button
-		baseStyles={'bg-red-600 hover:bg-red-700'}
-		{...props}>
-		{children}
-	</Button>
-}
-
-export function ButtonSuccess({children, ...props}:ButtonProps) {
-	return <Button
-		baseStyles={'bg-green-600 hover:bg-green-700'}
-		{...props}>
-		{children}
-	</Button>
-}
+export const fieldClassName = 'border p-1.5 rounded-md bg-gray-200';
 
 export function LabeledField({
 	label,
@@ -71,26 +28,8 @@ export function LabeledField({
 		<span className='text-sm pb-1'>{
 			label
 		}</span>
-		<span className='border p-1.5 rounded-md mb-4 bg-gray-200'>{
+		<span className={fieldClassName}>{
 			children
 		}</span>
 	</label>;
-}
-
-export function DangerLink({
-	children,
-	className='',
-	props
-}:{
-	children: React.ReactNode
-	className?: string,
-	props?: React.ButtonHTMLAttributes<HTMLButtonElement>
-}){
-	return <button
-		className={`border-none p-none m-none text-red-500
-			hover:underline text-left ${className}`}
-		{...props}
-	>{
-		children
-	}</button>
 }
