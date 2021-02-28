3652575de2566f849bb770cd27614e5231301459
Max Patiiuk <max@patii.uk>
2021-02-28 04:25:12 +0200

    Move global localization strings to the `config` directory

---
+++ const/commonStrings.ts
@@ -0,0 +1,13 @@
+import { LanguageStringsStructure } from '../lib/languages';
+
+const commonStrings: LanguageStringsStructure<{
+	yes: string,
+	no: string,
+}> = {
+	'en-US': {
+		yes: 'Yes',
+		no: 'No'
+	},
+};
+
+export default commonStrings;
